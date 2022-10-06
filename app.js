2ce63c54ec0e1cb760981bea89fb5a7457be4a66
“Jeahoung <jeahounghong@gmail.com>
2022-10-06 09:01:42 -0400

    add ingredients

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const users = require("./routes/api/user")
+const users = require("./routes/api/users")
 
 const app = express();
 const db = require('./config/keys').mongoURI;

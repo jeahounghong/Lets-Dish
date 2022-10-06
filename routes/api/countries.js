86a8dea736459e579fa0d7e909b866312007071c
“Jeahoung <jeahounghong@gmail.com>
2022-10-06 12:43:33 -0400

    routes for countries

--- routes/api/countries.js
+++ routes/api/countries.js
@@ -4,6 +4,10 @@ const Country = require("../../models/Country");
 
 router.get("/test", (req,res) => res.json({mes: "This is the countries route."}))
 
-
+router.post("/", (req,res) => {
+    const newCountry = new Country(req.body);
+    newCountry.recipes = [];
+    newCountry.save().then(country => res.json(country))
+})
 
 module.exports = router;

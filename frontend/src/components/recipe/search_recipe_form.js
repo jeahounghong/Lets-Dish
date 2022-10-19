e50d2ed10a2ececc10318e7784194543d7dbf0e6
“Jeahoung <jeahounghong@gmail.com>
2022-10-18 20:51:07 -0400

    search recipe form edit

--- frontend/src/components/recipe/search_recipe_form.js
+++ frontend/src/components/recipe/search_recipe_form.js
@@ -3,15 +3,32 @@ import './search_recipe_form.scss'
 
 class SearchRecipeForm extends React.Component {
 
+    constructor(props){
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+    }
 
     componentDidMount(){
         this.props.fetchRecipes();
         this.props.fetchIngredients();
     }
 
+
+
     render(){
         return <div className='search-recipe-container'>
             Search Recipes
+            <form onSubmit={this.handleSubmit}>
+
+
+                <div className="submit-container">
+                    <input type="submit" name="" value="Search Recipe!"/>
+                </div>
+            </form>
         </div>
     }
 }
